e4ee7b7c9fb41d9389a8820c15bda4b29a994750
Wannn Sion <wannnsion95@gmail.com>
2025-08-05 19:12:34 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -11,27 +11,27 @@ int main() {
     cout << "Masukkan NRP Anda: ";
     cin >> nomor_id;
 
-    if (nomor_id == 133100001) {
+    if (nomor_id == 4395001) {
         cout << "Nama: Fauzan Wahyu" << endl;
-    } else if (nomor_id == 133100002) {
+    } else if (nomor_id == 43950002) {
         cout << "Nama: Safran Matondang" << endl;
-    } else if (nomor_id == 133100003) {
+    } else if (nomor_id == 43950003) {
         cout << "Nama: Abd Jabbar" << endl;
-    } else if (nomor_id == 133100004) {
+    } else if (nomor_id == 43950004) {
         cout << "Nama: Sakinah Salsabila" << endl;
-    } else if (nomor_id == 133100005) {
+    } else if (nomor_id == 43950005) {
         cout << "Nama: Nabilah Rahmadani" << endl;
-    } else if (nomor_id == 133100006) {
+    } else if (nomor_id == 43950006) {
         cout << "Nama: Muhammad Fadlan" << endl;
-    } else if (nomor_id == 133100007) {
+    } else if (nomor_id == 43950007) {
         cout << "Nama: Muhammad Zulhamdi" << endl;
-    } else if (nomor_id == 133100008) {
+    } else if (nomor_id == 43950008) {
         cout << "Nama: Muhammad Ikhsan" << endl;
-    } else if (nomor_id == 133100009) {
+    } else if (nomor_id == 43950009) {
         cout << "Nama: Rayna Kayla" << endl;
-    } else if (nomor_id == 133100010) {
+    } else if (nomor_id == 43950010) {
         cout << "Nama: Anita Khairani" << endl;
-    } else if (nomor_id == 133100011) {
+    } else if (nomor_id == 43950011) {
         cout << "Nama: Mhd. Ridwan" << endl;
     } else {
         cout << "Invalid, harap masukkan nomor ID yang sesuai" << endl;
